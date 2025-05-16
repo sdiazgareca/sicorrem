f995192f3c07ba3ea0207bc775edbe9cebb44783
Sergio <sdiazgareca@gmail.com>
2025-05-15 23:05:12 -0400

    UF DB

--- Migraciones/20250514-uf.sql
+++ Migraciones/20250514-uf.sql
@@ -16,3 +16,54 @@ SELECT
     uf.mes as id_mes
 FROM UF uf
 INNER JOIN meses m ON m.NUMERO = uf.mes;
+
+CREATE OR REPLACE VIEW contrata_vista AS
+SELECT
+  contratos.ajuste AS ajuste,
+  contratos.f_pago AS f_pago,
+  f_pago.descripcion AS des_fpago,
+  empresa.empresa AS empresa,
+ROUND(
+  CASE
+    WHEN planes.id_tipo_moneda = 1 THEN planes.v_incor
+    WHEN uf.valor IS NOT NULL THEN planes.v_incor * uf.valor
+    ELSE 0
+  END, 2
+) AS v_incor,
+  planes.cod_plan AS cod_plan,
+  planes.tipo_plan AS tipo_plan,
+  planes.desc_plan AS desc_plan,
+  contratos.secuencia AS secuencia,
+  valor_plan.valor AS mensualidad,
+  titulares.nro_doc AS titular,
+  titulares.nombre1 AS nombre1,
+  titulares.nombre2 AS nombre2,
+  titulares.apellido AS apellido,
+  contratos.num_solici AS num_solici,
+  contratos.d_pago AS d_pago,
+  e_contrato.descripcion AS e_contrato_des,
+  e_contrato.cod AS e_contrato,
+  planes.n_inco_grat AS n_inco_grat,
+  uf.valor AS valor_uf,
+  planes.id_tipo_moneda AS id_tipo_moneda
+FROM contratos
+LEFT JOIN planes 
+  ON planes.cod_plan = contratos.cod_plan 
+  AND planes.tipo_plan = contratos.tipo_plan
+LEFT JOIN empresa 
+  ON empresa.nro_doc = contratos.empresa
+LEFT JOIN valor_plan 
+  ON valor_plan.cod_plan = contratos.cod_plan
+  AND valor_plan.tipo_plan = planes.tipo_plan
+  AND valor_plan.secuencia = contratos.secuencia
+LEFT JOIN titulares 
+  ON titulares.nro_doc = contratos.titular
+JOIN e_contrato 
+  ON contratos.estado = e_contrato.cod
+JOIN f_pago 
+  ON f_pago.codigo = contratos.f_pago
+LEFT JOIN uf ON 1=1;
+
+
+ALTER TABLE planes
+MODIFY COLUMN v_incor DECIMAL(10,2) NULL;
